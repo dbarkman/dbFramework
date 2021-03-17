4db08fb8fc26f3a6d516afeeb7d973780f1d9a4f
David Barkman <david.barkman13@gmail.com>
2021-03-16 17:50:47 -0700

    setup response logging in the Curl controller class. setup Curl controller class to either send back just the response or an array with the response and the status.

--- controllers/Curl.class.php
+++ controllers/Curl.class.php
@@ -9,24 +9,29 @@
 class Curl
 {
 	private $_logger;
+	public $debug;
 
 	public function __construct($logger)
 	{
 		$this->_logger = $logger;
 	}
 
-	protected function runCurl($requestMethod, $url, $headers = null, $userpwd = null, $fields = null)
+	protected function runCurl($requestMethod, $url, $headers = null, $userpwd = null, $fields = null, $returnStatus = false)
 	{
-		$ch = curl_init();
+        ob_start();
+        $out = fopen('/var/www/html/dbFramework/logs/curl.log', 'a');
+        fwrite($out, '-------------------- STARTING --------------------' . PHP_EOL);
+        fwrite($out, $url . PHP_EOL);
+        $ch = curl_init();
 		if ($requestMethod === 'GET') curl_setopt($ch, CURLOPT_HTTPGET, 1);
 		if ($requestMethod === 'POST') {
 			curl_setopt($ch, CURLOPT_POST, 1);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
 			curl_setopt($ch, CURLOPT_USERPWD, $userpwd);
-			if (!empty($headers)) {
-				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			}
 		}
+        if (!empty($headers)) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
 		if ($requestMethod === 'PUT') curl_setopt($ch, CURLOPT_PUT, 1);
 		if ($requestMethod === 'PUTJson') {
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($fields)));
@@ -38,9 +43,22 @@ class Curl
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		curl_setopt($ch, CURLOPT_STDERR, $out);
 		$output = curl_exec($ch);
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $response = array('status' => $status, 'output' => $output);
+
+        fwrite($out, '-------------------- ENDING --------------------' . PHP_EOL . PHP_EOL);
+        fclose($out);
+        $this->debug = ob_get_clean();
+
 		curl_close($ch);
-		$this->_logger->debug('CURL Output: ' . $output);
-		return $output;
+
+		if ($returnStatus == true) {
+		    return $response;
+        } else {
+            return $output;
+        }
 	}
 }
